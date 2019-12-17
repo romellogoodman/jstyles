7064476415a46359c64121c6ea5a02953d79bf5c
Romello Goodman <6355760+romellogoodman@users.noreply.github.com>
2019-12-17 14:15:28 -0500

    Got an initial api working

--- dev/src/index.js
+++ dev/src/index.js
@@ -3,30 +3,33 @@ import {render} from 'react-dom';
 
 import {css, styled} from 'jstyles';
 
-const mainClass = css({
-  'box-sizing': 'border-box',
-  'background-color': 'papayawhip',
-  padding: '80px',
-  width: '100%'
-});
+const mainClass = css`
+  box-sizing: border-box;
+  background-color: papayawhip;
+  padding: 80px;
+  width: 100%;
+`;
 
-const Div = styled('div', {
-  'background-color': 'white',
-  padding: '20px'
-});
+const Div = styled('div')`
+  background-color: white;
+  padding: 20px;
+`;
 
-const Title = styled('h2', {
-  color: 'pink',
-  'font-size': p => p.size || '20px',
-  'font-style': 'italic'
-});
+const Title = styled('h2')`
+  color: pink;
+  font-size: ${p => p.size || '20px'};
+  font-style: italic;
+`;
 
 const App = () => {
+  const [isBigType, setBigTitle] = React.useState(true);
+
   return (
     <main className={mainClass}>
       <Div>
-        <Title size="48px">hello world</Title>
+        <Title size={isBigType ? '48px' : '20px'}>hello world</Title>
       </Div>
+      <p onClick={() => setBigTitle(!isBigType)}>toggle size</p>
     </main>
   );
 };
